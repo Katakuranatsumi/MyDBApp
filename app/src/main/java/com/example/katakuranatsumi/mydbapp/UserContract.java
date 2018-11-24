83812f9a29078cadaeb04311d98f71bde4c66de7
Katakuranatsumi <usako7233@gmail.com>
2018-11-25 00:06:19 +0900

    2018/11/25 SQLデータ追加のための処理

--- app/src/main/java/com/example/katakuranatsumi/mydbapp/UserContract.java
+++ app/src/main/java/com/example/katakuranatsumi/mydbapp/UserContract.java
@@ -1,4 +1,13 @@
 package com.example.katakuranatsumi.mydbapp;
 
+import android.provider.BaseColumns;
+
 public final class UserContract {
+    public UserContract() {}
+
+    public static abstract class Users implements BaseColumns {
+     public static final String TABLE_NAME = "users";
+     public static final String COL_NAME = "name";
+     public static final String COL_SCORE = "score";
+    }
 }
